fbbcde6167ce7c1be26b61da70dd27570951c9d6
zhan-yl <zhan_yl@163.com>
2018-07-18 14:50:24 +0800

    changed repository from cloudnativego to zhan-yl

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/cloudfoundry-community/go-cfenv"
-	service "github.com/cloudnativego/gogo-service/service"
+	service "github.com/zhan-yl/gogo-service/service"
 )
 
 func main() {
